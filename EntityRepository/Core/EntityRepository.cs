02332aad1f969ee2c0782f6dac661a6d8447e799
DavidKloster <kloster.davidjonas@gmail.com>
2021-07-09 12:59:13 +0200

    Add project files.

---
+++ EntityRepository/Core/EntityRepository.cs
@@ -0,0 +1,203 @@
+﻿using EntityRepository.QueryableExtentions;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityRepository.Core
+{
+    /// <summary>
+    /// Generic Abstract Repository Class for easy and Uncomplicated Dataccess 
+    /// </summary>
+    /// <typeparam name="DatabaseContext">Gerneric Typeparameter of the Database context</typeparam>
+    /// <typeparam name="T">Generic Objecttype used by DataSet Router</typeparam>
+    public static class EntityRepository<DatabaseContext, T> where DatabaseContext : DbContext, IDisposable where T : class
+    {
+        private static DatabaseContext CreateDatabaseContextInstance()
+        {
+            return (DatabaseContext)Activator.CreateInstance(typeof(DatabaseContext), new object[] { });
+        }
+
+
+        public static async Task<List<T>> GetAllAsync(Func<T, bool> condition = null, int? maxResultSize = null, bool trackingEnabled = true)
+        {
+            return await Task.Run(() =>
+            {
+                using (var context = CreateDatabaseContextInstance())
+                {
+                    return context.RouteByEntity<T>().AppendMaxRowCount(maxResultSize).AppendTracking(trackingEnabled).AppendCondition(condition).ToList();
+                }
+            });
+        }
+
+        public static List<T> GetAll(Func<T, bool> condition = null, int? maxResultSize = null)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+                if (condition == null)
+                {
+                    return context.RouteByEntity<T>().AppendMaxRowCount(maxResultSize).AsNoTracking().ToList();
+                }
+                else
+                {
+                    return context.RouteByEntity<T>().AppendMaxRowCount(maxResultSize).AsNoTracking().Where(condition).ToList();
+                }
+            }
+        }
+
+        public static async Task<T> GetSingleAsync(Func<T, bool> condition = null)
+        {
+            return await Task.Run(() =>
+            {
+
+                using (var context = CreateDatabaseContextInstance())
+                {
+                    if (condition == null)
+                    {
+                        return context.RouteByEntity<T>().FirstOrDefault();
+                    }
+                    else
+                    {
+                        return context.RouteByEntity<T>().Where(condition).FirstOrDefault();
+                    }
+                }
+            });
+        }
+
+        public static T GetSingle(Func<T, bool> condition = null)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+                if (condition == null)
+                {
+                    return context.RouteByEntity<T>().FirstOrDefault();
+                }
+                else
+                {
+                    return context.RouteByEntity<T>().Where(condition).FirstOrDefault();
+                }
+            }
+        }
+
+        public static async Task<T> CreateAsync(T obj)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+                context.RouteByEntity<T>().Add(obj);
+                await context.SaveChangesAsync();
+                return obj;
+            }
+        }
+
+        public static T Create(T obj)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+                context.RouteByEntity<T>().Add(obj);
+                context.SaveChanges();
+                return obj;
+            }
+        }
+
+        public static async Task<T> ModifyAsync(Func<T, bool> condition, T obj)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+                var objs = context.RouteByEntity<T>().Where(condition).ToList();
+                if (objs.Count > 1) throw new NotSupportedException($"Condition {nameof(condition)} does not return Single Value");
+                if (!objs.Any()) return null;
+
+                T objtoedit = objs.First();
+
+                obj.CopyProperties(objtoedit);
+
+                await context.SaveChangesAsync();
+                return objtoedit;
+            }
+        }
+
+        public static T Modify(Func<T, bool> condition, T obj)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+                var objs = context.RouteByEntity<T>().Where(condition).ToList();
+                if (objs.Count > 1) throw new NotSupportedException($"Condition {nameof(condition)} does not return Single Value");
+                if (!objs.Any()) return null;
+
+                T objtoedit = objs.First();
+
+                obj.CopyProperties(objtoedit);
+
+                context.SaveChanges();
+                return objtoedit;
+            }
+        }
+
+        public static async Task<bool> DeleteAsync(Func<T, bool> condition)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+                List<T> objstodelete = context.RouteByEntity<T>().Where(condition).ToList();
+
+                foreach (var ob in objstodelete)
+                {
+                    context.RouteByEntity<T>().Remove(ob);
+                }
+
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+
+
+
+        public static async Task<bool> DeleteAsync(T obj)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+
+                context.RouteByEntity<T>().Remove(obj);
+
+                await context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+
+        public static bool Delete(Func<T, bool> condition)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+                List<T> objstodelete = context.RouteByEntity<T>().Where(condition).ToList();
+
+                foreach (var ob in objstodelete)
+                {
+                    context.RouteByEntity<T>().Remove(ob);
+                }
+
+                context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+
+        public static bool Delete(T obj)
+        {
+            using (var context = CreateDatabaseContextInstance())
+            {
+
+                context.RouteByEntity<T>().Remove(obj);
+
+                context.SaveChangesAsync();
+
+                return true;
+            }
+        }
+
+
+    }
+
+}
---
+++ EntityRepository/QueryableExtentions/QueryableExtention.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EntityRepository.QueryableExtentions
+{
+    internal static class QueryableExtention
+    {
+        /// <summary>
+        /// DbContext Extention - Method gets and returns the first property which matches the Generic Type DbSet<T> in the extended DbContext
+        /// </summary>
+        /// <typeparam name="EntityType">class of specified DbSet</typeparam>
+        /// <param name="dbContext">Extended DbSet</param>
+        /// <returns>First found DbSet</returns>
+        public static DbSet<EntityType> RouteByEntity<EntityType>(this DbContext dbContext) where EntityType : class
+        {
+            if (dbContext == null) throw new ArgumentNullException($"The given context is null. DbSet<{nameof(EntityType)}> not found");
+
+            var contextType = dbContext.GetType();
+            var props = contextType.GetProperties();
+
+            if (!props.Any()) throw new ArgumentException($"The given context does not define DbSets. DbSet<{nameof(EntityType)}> not found");
+
+            var matchProps = props.Where(x => x.PropertyType.Equals(typeof(DbSet<EntityType>))).FirstOrDefault();
+
+            if (matchProps != null)
+            {
+                return matchProps.GetValue(dbContext) as DbSet<EntityType>;
+            }
+            else
+            {
+                throw new ArgumentException($"The given entity route does not exists. DbSet<{nameof(EntityType)}> not found");
+            }
+
+        }
+
+        /// <summary>
+        /// Appends the Query with Optional Tracking Statement
+        /// </summary>
+        /// <typeparam name="T">Gerneric Type of UsageBase</typeparam>
+        /// <param name="query">Ef6 Queryable</param>
+        /// <param name="enabled"></param>
+        /// <returns></returns>
+        public static IQueryable<T> AppendTracking<T>(this IQueryable<T> query, bool enabled) where T : class
+        {
+            if (enabled)
+            {
+                return query;
+            }
+            else
+            {
+                return query.AsNoTracking();
+            }
+        }
+
+        /// <summary>
+        /// Appends the Take Statement to the iQueryable defaults to Null which means all
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="rowlimit"></param>
+        /// <returns></returns>
+        public static IQueryable<T> AppendMaxRowCount<T>(this IQueryable<T> query, int? rowlimit = null) where T : class
+        {
+            if (!rowlimit.HasValue)
+            {
+                return query;
+            }
+            else
+            {
+                return query.Take(rowlimit.Value);
+            }
+
+        }
+
+        /// <summary>
+        /// Appends whercondition to the Iqueryable statement 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        public static IEnumerable<T> AppendCondition<T>(this IQueryable<T> query, Func<T, bool> condition) where T : class
+        {
+            if (condition == null)
+            {
+                return query;
+            }
+            else
+            {
+                return query.Where(condition);
+            }
+        }
+
+
+
+
+    }
+}
